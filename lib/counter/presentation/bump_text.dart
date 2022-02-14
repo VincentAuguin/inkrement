fdbd17e10ce086647797066cf6821301aaae563e
Vincent Auguin <vauguin@ippon.fr>
2022-02-15 00:25:04 +0100

    fix(counter): simplify animation on bump to avoid glitch

--- lib/counter/presentation/bump_text.dart
+++ lib/counter/presentation/bump_text.dart
@@ -1,5 +1,3 @@
-import 'dart:async';
-
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 
@@ -17,65 +15,49 @@ class BumpIntText extends StatefulWidget {
 
 class _BumpIntTextState extends State<BumpIntText>
     with SingleTickerProviderStateMixin {
-  static const Duration animationDuration = Duration(milliseconds: 750);
+  static const Duration animationDuration = Duration(milliseconds: 300);
 
   late final AnimationController _controller = AnimationController(
     duration: animationDuration,
     vsync: this,
   );
+  late final Animation<double> _animation = CurvedAnimation(
+    parent: _controller,
+    curve: Curves.easeOut,
+  );
   late final Animation<Offset> _offsetAnimation = Tween<Offset>(
     begin: Offset.zero,
     end: const Offset(0.0, -0.5),
-  ).animate(CurvedAnimation(
-    parent: _controller,
-    curve: Curves.easeOutExpo,
-  ));
-  late final Animation<double> _previousValueOpacityAnimation = Tween<double>(
+  ).animate(_animation);
+  late final Animation<double> _opacityAnimation = Tween<double>(
     begin: 1.0,
     end: 0.0,
-  ).animate(CurvedAnimation(
-    parent: _controller,
-    curve: Curves.easeOutExpo,
-  ));
-  late bool _widgetNeverBumped;
-  late String _previousValue;
-  late double _actualValueOpacity;
+  ).animate(_animation);
+
+  late String _value;
 
   void _bump() {
-    // Trigger bump animations
-    if (_actualValueOpacity == 0.0) {
-      Timer(
-          Duration(
-              milliseconds: (animationDuration.inMilliseconds * 0.3).toInt()),
-          () {
-        setState(() {
-          _actualValueOpacity = 1.0;
-          _widgetNeverBumped = false;
-        });
+    _controller.forward().then((value) {
+      setState(() {
+        _value = widget.value.toString();
       });
-      if (!_widgetNeverBumped) {
-        _controller
-          ..reset()
-          ..forward();
-      }
-    }
+      _controller.reset();
+    });
   }
 
   @override
   void initState() {
     super.initState();
-    _previousValue = "";
-    _actualValueOpacity = 1.0;
-    _widgetNeverBumped = true;
+    _value = widget.value.toString();
   }
 
   @override
   void didUpdateWidget(covariant BumpIntText oldWidget) {
     super.didUpdateWidget(oldWidget);
     setState(() {
-      _previousValue = oldWidget.value.toString();
-      _actualValueOpacity = 0.0;
+      _value = oldWidget.value.toString();
     });
+    _bump();
   }
 
   @override
@@ -86,33 +68,19 @@ class _BumpIntTextState extends State<BumpIntText>
 
   @override
   Widget build(BuildContext context) {
-    _bump();
-
     TextStyle effectiveTextStyle =
         widget.textStyle ?? DefaultTextStyle.of(context).style;
 
     return Center(
-        child: Stack(
-      children: [
-        FadeTransition(
-            opacity: _previousValueOpacityAnimation,
+        child: FadeTransition(
+            opacity: _opacityAnimation,
             child: SlideTransition(
                 position: _offsetAnimation,
                 child: AutoSizeText(
-                  _previousValue.toString(),
+                  _value.toString(),
                   style: effectiveTextStyle,
                   textAlign: TextAlign.center,
                   maxLines: 1,
-                ))),
-        Opacity(
-            opacity: _actualValueOpacity,
-            child: AutoSizeText(
-              widget.value.toString(),
-              style: effectiveTextStyle,
-              textAlign: TextAlign.center,
-              maxLines: 1,
-            ))
-      ],
-    ));
+                ))));
   }
 }
